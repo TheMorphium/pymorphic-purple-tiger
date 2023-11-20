20025b0d29a0d09a554b1f6ad7f15e52d086e413
Sourcery AI <>
2023-11-20 13:49:30 +0000

    'Refactored by Sourcery'

--- examples/demo.py
+++ examples/demo.py
@@ -1,10 +1,5 @@
 import random
 from pymorphic_purple_tiger.pymorphic_purple_tiger import animal_hash
 
-string = ''
-i = 0
-while i < 100:
-  string += f'{random.randint(0,9)}'
-  i += 1
-
+string = ''.join(f'{random.randint(0, 9)}' for _ in range(100))
 print(animal_hash(string))
--- pymorphic_purple_tiger/compress.py
+++ pymorphic_purple_tiger/compress.py
@@ -15,15 +15,9 @@ def compress(bytes, target):
     i, segments = 0, []
     while i < target:
         start_point = i * seg_size
-        if i + 1 == target:
-            end_point = length  # Catch any left-over bytes in the last segment.
-        else:
-            end_point = (i + 1) * seg_size
+        end_point = length if i + 1 == target else (i + 1) * seg_size
         slice = bytes[start_point:end_point]
         segments.append(slice)
         i += 1
 
-    checksums = [reduce(lambda acc, curr: acc ^ curr, segment) for segment in segments]
-
-
-    return checksums
+    return [reduce(lambda acc, curr: acc ^ curr, segment) for segment in segments]
